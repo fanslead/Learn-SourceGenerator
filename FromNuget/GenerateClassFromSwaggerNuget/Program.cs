55c97ebdf4a7bbb1801d01dc73606e53ae1e5ca0
jfan liu <136590076@qq.com>
2024-04-11 12:23:17 +0800

    Update

---
+++ FromNuget/GenerateClassFromSwaggerNuget/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenerateClassFromSwaggerNuget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
